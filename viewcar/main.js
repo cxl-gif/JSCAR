ddd5d3b0f5627cab6c5dc0a02d8a6e132891cc24
sara <sara.nakiri.15@gmail.com>
2026-03-25 07:02:34 +0100

    seconde commit

--- viewcar/main.js
+++ viewcar/main.js
@@ -1,38 +1,17 @@
-
-const button = document.getElementById("bookBtn");
-
-button.addEventListener("click", function () {
-
-    if (daysSpan.textContent == 0) {
-        return;
-    }
-
-    localStorage.setItem("days", daysSpan.textContent);
-    localStorage.setItem("total", totalSpan.textContent);
-
-    window.location.href = "/payementpg/index.html";
-});
-
 const startInput = document.getElementById("start");
 const endInput = document.getElementById("end");
 
-document.getElementById("bookBtn").addEventListener("click", function () {
-
+document.querySelector("button").addEventListener("click", function () {
     const start = new Date(startInput.value);
     const end = new Date(endInput.value);
 
-    let diffTime = end - start;
-    let diffDays = diffTime / (1000 * 60 * 60 * 24);
+    const diffTime = end - start;
+    const days = diffTime / (1000 * 60 * 60 * 24);
 
-    if (diffDays <= 0) diffDays = 1;
+    const total = days * 150;
 
-    const pricePerDay = 150;
-    const total = diffDays * pricePerDay;
-
-    // 🔥 نخزنو
-    localStorage.setItem("days", diffDays);
+    localStorage.setItem("days", days);
     localStorage.setItem("total", total);
 
-    // 🔥 تمشي لـ payment
     window.location.href = "/payementpg/index.html";
 });
