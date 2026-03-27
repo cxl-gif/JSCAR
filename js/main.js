cdcec18dcff2e715c362b284026c3895a767054c
sara <sara.nakiri.15@gmail.com>
2026-03-27 10:52:36 +0100

    seconde commit

--- js/main.js
+++ js/main.js
@@ -70,3 +70,16 @@ if (resetFiltersBtn) {
 
 
 
+function saveCar(name, img, price, fuel, seats, desc, images,transmision,category) {
+    localStorage.setItem("carName", name);
+    localStorage.setItem("carImg", img);
+    localStorage.setItem("carPrice", price);
+    localStorage.setItem("carFuel", fuel);
+    localStorage.setItem("carSeats", seats);
+    localStorage.setItem("carDesc", desc);
+    localStorage.setItem("carImages", JSON.stringify(images));
+    localStorage.setItem("cartrans", transmision);
+    localStorage.setItem("carcategory", category);
+
+}
+
--- viewcar/main.js
+++ viewcar/main.js
@@ -1,7 +1,10 @@
+
 const startInput = document.getElementById("start");
 const endInput = document.getElementById("end");
+const btn = ocument.querySelector("button");
 
-document.querySelector("button").addEventListener("click", function () {
+btn.addEventListener("click", function () {
+
     const start = new Date(startInput.value);
     const end = new Date(endInput.value);
 
@@ -15,3 +18,27 @@ document.querySelector("button").addEventListener("click", function () {
 
     window.location.href = "/payementpg/index.html";
 });
+
+
+const name = localStorage.getItem("carName");
+const img = localStorage.getItem("carImg");
+const price = localStorage.getItem("carPrice");
+const fuel = localStorage.getItem("carFuel");
+const seats = localStorage.getItem("carSeats");
+const desc = localStorage.getItem("carDesc");
+const transmission = localStorage.getItem("cartrans");
+const category = localStorage.getItem("carcategory");
+
+
+document.getElementById("car-transmission").textContent = transmission;
+document.getElementById("car-category").textContent = category;
+
+document.getElementById("car-name").textContent = name;
+document.getElementById("car-sub").textContent = name + " 2024";
+document.getElementById("car-price").textContent = "$" + price;
+
+document.getElementById("main-car").src = img;
+
+document.getElementById("car-fuel").textContent = fuel;
+document.getElementById("car-seats").textContent = seats;
+document.getElementById("car-description").textContent = desc;
